9c15507f65e053534bdf48b409cbfe8b5602bdec
Pedro Ferreira <fazesse1998@gmail.com>
2024-01-15 11:35:40 +0000

    calculadora v1

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+"""
+Desenvolver calculadora
+"""
+
+def soma(num1, num2):
+    return num1 + num2
+
+num1 =int(input("1 num:"))
+num2 =int(input("2 num:"))
+
+somar = soma(num1, num2)
+
+print(f"{num1} + {num2} = {somar}")
+
+
